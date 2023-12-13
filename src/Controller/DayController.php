d061e67ff92394091258ca88384c1fba86fd4258
RedaMG1 <mreda.elalaoui1@gmail.com>
2023-12-13 14:11:02 +0100

    02

--- src/Controller/DayController.php
+++ src/Controller/DayController.php
@@ -3,27 +3,59 @@
 namespace App\Controller;
 
 use App\Repository\DayRepository;
+use App\Repository\DayStartRepository;
+use App\Repository\ProgramRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class DayController extends AbstractController
 {
-    #[Route('/day', name: 'app_day')]
-    public function index(DayRepository $dayRepository): Response
+    #[Route('/day/{id}', name: 'app_day')]
+    public function index(DayRepository $dayRepository,$id): Response
     {
-        $days = $dayRepository->findAll();
+        $days = $dayRepository->findBy(['program'=>$id]);
         return $this->render('day/index.html.twig', [
             'days' => $days,
         ]);
     }
 
-    #[Route('/day/start', name: 'day_start')]
-    public function start(DayRepository $dayRepository): Response
+    #[Route('/day/start/{id}', name: 'day_start')]
+    public function start(
+        DayRepository $dayRepository,DayStartRepository $dayStartRepository,
+        ProgramRepository $programRepository,$id
+    ): Response
     {
         $days = $dayRepository->findAll();
+        $programs = $programRepository->findAll();
+        $dayStarts = $dayStartRepository->findBy(['day'=>$id]);
         return $this->render('day/start.html.twig', [
+            'programs' => $programs,
+            'dayStarts' => $dayStarts,
             'days' => $days,
+        ]);
+    }
+
+    #[Route('/dayStart/filter/{id}', name: 'day_filter')]
+    public function filterByDay(
+        $id,
+        Request $request,DayRepository $dayRepository,
+        DayStartRepository $dayStartRepository,ProgramRepository $programRepository,
+    ): Response {
+  
+        $dayStartFilter = $dayStartRepository->findByDay($id);
+        $programs = $programRepository->findAll();
+        // $categorys = $categoryRepository->findAll();
+        $days = $dayRepository->findAll();
+        $dayStarts = $dayStartRepository->findBy(['day'=>$id]);
+        // dd($categorys);
+        return $this->render('day/start.html.twig', [
+            'dayStartFilter' => $dayStartFilter,
+            // 'categorys' => $categorys,
+            'programs' => $programs,
+            'days' => $days,
+            'dayStarts' => $dayStarts,
         ]);
     }
 }
